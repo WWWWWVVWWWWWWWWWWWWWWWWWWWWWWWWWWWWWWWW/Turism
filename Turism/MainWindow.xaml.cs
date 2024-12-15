7069e6372de2aca7713cdebe7dc3a35f47256956
Александр <@DESKTOP-6SCUKPQ>
2024-12-16 00:35:09 +0300

    Добавьте файлы проекта.

---
+++ Turism/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Turism
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sing_Click(object sender, RoutedEventArgs e)
+        {
+            string username = login.Text;
+            string password = pass.Password;
+
+            if (username == "Admin" && password == "12345")
+            {
+                FrmMain.Navigate(new Page1());
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+
+            }
+        }
+    }
+}
